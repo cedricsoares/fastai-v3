674e3fd6cdef60bb8146372a325f2ab507159766
cedricsoares <cedricsoares@me.com>
2019-04-19 15:26:10 +0000

    Bug fix on server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/file/d/1dEn_bpbYc8y6kMoX5vhiwV07q3QVNAfu/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1dEn_bpbYc8y6kMoX5vhiwV07q3QVNAfu'
 export_file_name = 'export.pkl'
 
 classes = ['good tackle', 'bad tackle']
